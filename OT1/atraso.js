cb17cdf024d057c9c3bf1c4213dd2cba5181a39d
Kadnys07 <kadnysm|@gmail.com>
2025-07-30 18:55:49 -0300

    First commit

---
+++ OT1/atraso.js
@@ -0,0 +1,8 @@
+// atraso.js
+console.log("Iniciando o script...");
+
+setTimeout(() => {
+  console.log("Esta mensagem foi exibida após 3 segundos.");
+}, 3000);
+
+console.log("Script finalizado (ou  quase!).");
